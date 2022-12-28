c3794ae258d8abb6a50b249de8703853e77fc3e7
Jisu Choi <redgem92@gmail.com>
2022-12-28 20:59:49 +0900

    네이게이션 디자인 추가

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -3,12 +3,6 @@ import "./Navigation.css";
 const Header = () => {
   return (
     <div className="open-code-generator-header">
-      <img
-        src={process.env.PUBLIC_URL + "/assets/open-code-generator-icon.png"}
-        alt="Open Code Generator"
-      >
-      </img>
-      <h3>open-code-genrator</h3>
     </div>
   );
 }
