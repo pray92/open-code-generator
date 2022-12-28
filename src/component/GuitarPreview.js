05266d35fd5cb777070dfd79787fac516bccfec0
Jisu Choi <redgem92@gmail.com>
2022-12-28 21:00:19 +0900

    기타 프리뷰 구현

--- src/component/GuitarPreview.js
+++ src/component/GuitarPreview.js
@@ -16,20 +16,22 @@ import "./GuitarPreview.css";
 
 const GuitarPreview = () => {
   return (
-    <div className="guitar-preview">
-      <img src={Pitch} alt="Start Pitch Set" />
-      <img src={Fret1} alt="Fret-1" />
-      <img src={Fret2} alt="Fret-2" />
-      <img src={Fret3} alt="Fret-3" />
-      <img src={Fret4} alt="Fret-4" />
-      <img src={Fret5} alt="Fret-5" />
-      <img src={Fret6} alt="Fret-6" />
-      <img src={Fret7} alt="Fret-7" />
-      <img src={Fret8} alt="Fret-8" />
-      <img src={Fret9} alt="Fret-9" />
-      <img src={Fret10} alt="Fret-10" />
-      <img src={Fret11} alt="Fret-11" />
-      <img src={Fret12} alt="Fret-12" />
+    <div className="guitar-preview-container">
+      <div className="guitar-preview">
+        <img src={Pitch} alt="Start Pitch Set" />
+        <img src={Fret1} alt="Fret-1" />
+        <img src={Fret2} alt="Fret-2" />
+        <img src={Fret3} alt="Fret-3" />
+        <img src={Fret4} alt="Fret-4" />
+        <img src={Fret5} alt="Fret-5" />
+        <img src={Fret6} alt="Fret-6" />
+        <img src={Fret7} alt="Fret-7" />
+        <img src={Fret8} alt="Fret-8" />
+        <img src={Fret9} alt="Fret-9" />
+        <img src={Fret10} alt="Fret-10" />
+        <img src={Fret11} alt="Fret-11" />
+        <img src={Fret12} alt="Fret-12" />
+      </div>
     </div>
   );
 }
